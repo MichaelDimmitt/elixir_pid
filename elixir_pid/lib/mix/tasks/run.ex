2e0528ba5d0cf1e0ae2e09b5163c38fb5a63102c
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-12 09:47:04 -0400

    examples from the elixir process documentation.

--- elixir_pid/lib/mix/tasks/run.ex
+++ elixir_pid/lib/mix/tasks/run.ex
@@ -1,8 +1,38 @@
 defmodule Mix.Tasks.RunTask do
   use Mix.Task
-  @shortdoc "Simply calls ElixirPidExamples.filePid/1 and ElixirPidExamples.filePid/1 functions."
+  @shortdoc "calls ElixirPidExamples.filePid/1 and ElixirPidExamples.filePid/1 functions."
   def run(_) do
     ElixirPidExamples.filePid('hello') |> IO.inspect()
     ElixirPidExamples.file('hello') |> IO.inspect()
+    from_the_docs()
+  end
+
+  @shortdoc "implementation of process examples from https://elixir-lang.org/getting-started/processes.html"
+  def from_the_docs() do
+    pid = spawn fn -> 1 + 2 end
+    qqq = quote do: pid
+    qqq|> IO.inspect()
+    pid |> IO.inspect()
+    Process.alive?(pid) |> IO.inspect()
+    self() |> IO.inspect()
+    Process.alive?(self()) |> IO.inspect()
+    send self(), {:hello, "world"} |> IO.inspect()
+
+    receive do
+      {:hello, msg}  -> msg
+      {:world, _msg} -> "won't match"
+    after
+      1_000 -> "nothing after 1s"
+    end |> IO.inspect()
+
+    parent = self() |> IO.inspect()
+    spawn fn -> send(parent, {:hello, self()}) end |> IO.inspect()
+
+    receive do
+      {:hello, pid} -> "Got hello from #{inspect pid}"
+    end |> IO.inspect()
+
+    send self(), :hello |> IO.inspect()
   end
 end
+
